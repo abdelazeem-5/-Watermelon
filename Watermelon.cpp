7768059dddea191bf1b8dbe12170cdf5b4a3012b
abdelazeem-5 <154232833+abdelazeem-5@users.noreply.github.com>
2024-04-10 19:17:11 +0200

    Update Watermelon.cpp

--- Watermelon.cpp
+++ Watermelon.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
+    int w;
+    cin >> w;
 
+    if (w % 2 == 0 || (w > 2 && (w - 2) % 2 == 0)) 
+    {
+      cout << "YES" << endl;
+    } 
+    else
+    {
+      cout << "NO" << endl;
+    }
 
     return 0;
 }
